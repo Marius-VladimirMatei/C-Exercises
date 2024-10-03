b02678f4c467518ac902f7e0dd7151f2e101cdec
Marius-VladimirMatei <052583@edu.szf.at>
2024-10-03 10:44:26 +0200

    Add files via upload

---
+++ CFILES/8 Rectange Area LOOP.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+
+// Rectangle area with user choice to continue
+
+void main() {
+
+
+    float length;
+    float width;
+    char choice;
+
+    do {
+       
+        // user input and compiler read
+        float length;
+        printf_s("Enter the Length: \n");
+        scanf_s("%f", &length);
+
+        float width;
+        printf_s("Enter the Width: \n");
+        scanf_s("%f", &width);
+
+        // formula and output
+        float area = (length * width);
+        printf_s("The area of your rectangle is: %.2f\n", area);
+
+
+        // clear input buffer
+        while (getchar() != '\n');
+
+
+        // prompt user for permission to continue
+        printf_s("Do you want to calculate further? (y/n): ");
+
+        // the white space before %c tells scanf_s to skip any leading whitespace and newlines
+        scanf_s("%c", &choice);
+
+    }
+
+    while (choice == 'y' || choice == 'Y');
+
+    printf_s("Thank you for using the program!\n");
+
+
+}
